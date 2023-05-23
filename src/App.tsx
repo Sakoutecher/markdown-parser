52d6875f54e5585133e75907b663e0fea8573513
hcampos <hcampospro@gmail.com>
2023-05-23 10:53:56 +0200

    ♻️ Use githubutton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+//Librairies
 import { ReactElement } from 'react'
+
+//Components
+import { GithubButton } from '#/GithubButton'
 
 const App = (): ReactElement => {
   return (
-    <div className='w-screen h-screen bg-zinc-300 flex flex-col justify-center items-center font-jetbrains'>
+    <div className='w-screen h-screen bg-zinc-300 flex flex-col justify-center items-center font-jetbrains relative'>
       <h1 className='uppercase text-2xl mb-8'>Markdown Parser</h1>
       <div className='w-4/5 h-3/4 flex justify-between items-center gap-6'>
         <textarea className='bg-zinc-400 w-3/6 h-full outline-none rounded resize-none py-2 px-4'></textarea>
         <div className='bg-zinc-400 w-3/6 h-full rounded'></div>
       </div>
+      <GithubButton />
     </div>
   )
 }
