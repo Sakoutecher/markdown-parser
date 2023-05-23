739fb8b3e081d8d04175d7551759ef0d26488beb
hcampos <hcampospro@gmail.com>
2023-05-23 11:24:19 +0200

    ♻️ Use the custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 //Librairies
 import { ReactElement } from 'react'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
 //Components
 import { GithubButton } from '#/GithubButton'
 import { TextArea } from '#/TextArea'
 
+//Hooks
+import { useTextStore } from './lib/hooks/useText'
+
 const App = (): ReactElement => {
+  const text = useTextStore((state) => state.text)
+
   return (
     <div className='w-screen h-screen bg-zinc-300 flex flex-col justify-center items-center font-jetbrains relative'>
       <h1 className='uppercase text-2xl mb-8'>Markdown Parser</h1>
       <div className='w-4/5 h-3/4 flex justify-between items-center gap-6'>
         <TextArea />
-        <div className='bg-zinc-400 w-3/6 h-full rounded'></div>
+        <ReactMarkdown
+          className='bg-zinc-400 w-3/6 h-full rounded py-2 px-4'
+          children={text}
+          remarkPlugins={[remarkGfm]}
+        />
       </div>
       <GithubButton />
     </div>
--- src/lib/components/TextArea/TextArea.tsx
+++ src/lib/components/TextArea/TextArea.tsx
@@ -1,5 +1,19 @@
+//Hooks
+import { useTextStore } from '../../hooks/useText'
+
 export const TextArea = () => {
+  const text = useTextStore((state) => state.text)
+  const setText = useTextStore((state) => state.setText)
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value)
+  }
+
   return (
-    <textarea className='bg-zinc-400 w-3/6 h-full outline-none rounded resize-none py-2 px-4'></textarea>
+    <textarea
+      value={text}
+      onChange={handleChange}
+      className='bg-zinc-400 w-3/6 h-full outline-none rounded resize-none py-2 px-4'
+    ></textarea>
   )
 }
