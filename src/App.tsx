e92e0153b6f6520fd9d081450d2d0a180112ebd6
hcampos <hcampospro@gmail.com>
2023-05-23 10:58:27 +0200

    🚚 TextArea into component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { ReactElement } from 'react'
 
 //Components
 import { GithubButton } from '#/GithubButton'
+import { TextArea } from '#/TextArea'
 
 const App = (): ReactElement => {
   return (
     <div className='w-screen h-screen bg-zinc-300 flex flex-col justify-center items-center font-jetbrains relative'>
       <h1 className='uppercase text-2xl mb-8'>Markdown Parser</h1>
       <div className='w-4/5 h-3/4 flex justify-between items-center gap-6'>
-        <textarea className='bg-zinc-400 w-3/6 h-full outline-none rounded resize-none py-2 px-4'></textarea>
+        <TextArea />
         <div className='bg-zinc-400 w-3/6 h-full rounded'></div>
       </div>
       <GithubButton />
